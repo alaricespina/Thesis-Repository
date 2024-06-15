cf72436cf74b43862753d6164ecda8f667ee4274
alaricespina <alaricsespina@gmail.com>
2024-06-15 20:33:25 +0800

    Rearrange

--- Deployment - Auto Regressive/DataCombiner.py
+++ Deployment - Auto Regressive/DataCombiner.py
@@ -3,12 +3,19 @@ import os
 
 listOfFiles = []
 folderName = "Raw Atmospheric Data"
+initial_file = None
+last_file = None
 for file in os.listdir(folderName):
-    if "2024" not in file:
-        print(file)
-        listOfFiles.append(pd.read_csv(os.path.join(folderName, file)))
-    
+    if initial_file == None:
+        initial_file = file.replace(".csv", "") 
 
-pd.concat(listOfFiles, ignore_index = True, sort = False ).to_csv("1998 to 2023 CombinedData.csv")
+    if "2024" not in file:
+        # print(file)
+        listOfFiles.append(pd.read_csv(os.path.join(folderName, file)))
+        last_file = file.replace(".csv", "")
+
+print("Initial File is:", initial_file, "Last File is:", last_file)
+
+pd.concat(listOfFiles, ignore_index = True, sort = False ).to_csv(f"{initial_file} to {last_file} CombinedData.csv")
 
 print("Done Concatenating")
