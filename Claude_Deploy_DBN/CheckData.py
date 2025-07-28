4b038eac46a8aba2bb8eb979942aacaf87da7e19
alaricespina <alaricsespina@gmail.com>
2025-07-29 04:19:37 +0800

    Clean Up Main Branch

--- Claude_Deploy_DBN/CheckData.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-feb_df = pd.read_csv("Data/Feb 2025 Hourly.csv")
-jan_df = pd.read_csv("Data/Jan 2025 Hourly.csv")
-
-# Combine the two pdf
-
--- Sensor Reference/dht11.py
+++
@@ -1,8 +0,0 @@
-import time
-import board
-import adafruit_dht
-
-sensor = adafruit_dht.DHT11(board.D4)
-print("Hatdog")
-print("Sensor Reading:", sensor.temperature)
-print("Sensor Humidity:", sensor.humidity)
